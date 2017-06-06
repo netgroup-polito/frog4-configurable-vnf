230fce6b3f181ff585147538976a877eb8e61c83
piscoroma <piscoroma@gmail.com>
2017-06-06 13:11:34 +0200

    removed old folders

--- dhcp/rest_api/api.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from flask_restplus import Api
-
-dhcp_blueprint = Blueprint('root', __name__)
-api = Api(dhcp_blueprint, version='1.0', title='Dhcp Server API', description='Dhcp Server API',
-          doc='/api_docs/')
--- firewall/dd_controller/element.py
+++
@@ -1,5 +0,0 @@
-class Element():
-    def __init__(self, url=None, advertise=None, period=None):
-        self.url = url
-        self.advertise = advertise
-        self.period = period
